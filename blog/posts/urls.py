f2816d5b6ad53221512279654e32c51848d4d9ee
Alex <99095805+AlexWT1@users.noreply.github.com>
2024-05-02 05:29:28 +0300

    complited main func

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 from .models import LikedPostsView
+from .views import DeleteCommentView
 
 urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('create/', views.create_post, name='post_create'),
     path('like_post/<int:post_id>/', views.like_post, name='like_post'),
     path('liked_posts/', LikedPostsView.as_view(), name='liked_posts'),
+    path('delete_comment/<int:comment_id>/', DeleteCommentView.as_view(), name='delete_comment'),
+    path('my_posts/', views.MyPostsView.as_view(), name='my_posts'),
+    path('liked-posts/', views.liked_posts, name='liked_posts'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 from .forms import CommentForm, PostForm
@@ -8,38 +10,53 @@ from .models import Post, Comment
 from django.contrib.auth.decorators import login_required
 
 
+class DeleteCommentView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        comment_id = kwargs['comment_id']
+        comment = Comment.objects.get(id=comment_id)
+        if request.user != comment.author:
+            # Возвращаем ошибку или редирект, если пользователь не автор комментария
+            return redirect('post_detail', pk=comment.post.pk)
+        comment.delete()
+        return redirect('post_detail', pk=comment.post.pk)
+
+
+class MyPostsView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.filter(author=request.user).order_by('-created_at')
+        return render(request, 'posts/my_posts.html', {'posts': posts, 'is_homepage': True})
+
+
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_at')
     form = PostForm()
     for post in posts:
         post.is_liked = post.is_liked_by_user(request.user)
-    return render(request, 'posts/post_list.html', {'posts': posts, 'form': form , 'is_homepage': True})
+    return render(request, 'posts/post_list.html', {'posts': posts, 'form': form, 'is_homepage': True})
 
 
 def post_detail(request, pk):
     try:
-        post = Post.objects.prefetch_related('comments').get(pk=pk)
-        comments = post.comments.all()
+        post = get_object_or_404(Post, pk=pk)
         if request.method == 'POST':
             form = CommentForm(request.POST)
             if form.is_valid():
                 comment = form.save(commit=False)
                 comment.post = post
+                comment.author = request.user
                 comment.save()
-                return redirect('post_detail', pk=pk)
+                return redirect('post_detail', pk=post.pk)
         else:
             form = CommentForm()
         context = {
             'post': post,
-            'comments': comments,
-            'form': form,
-            'is_homepage': False, # Устанавливаем is_homepage в False для детальных страниц постов
+            'comment_form': form,
+            'is_homepage': False,  # Устанавливаем is_homepage в False для детальных страниц постов
         }
         return render(request, 'posts/post_detail.html', context)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
-
 
 
 @login_required
@@ -68,3 +85,8 @@ def like_post(request, post_id):
         post.likes.add(request.user)
         message = "Liked"
     return JsonResponse({"message": message, "likes_count": post.likes.all().count()})
+
+
+def liked_posts(request):
+    liked_posts = Post.objects.filter(likes=request.user).order_by('-created_at')
+    return render(request, 'posts/liked_posts.html', {'posts': liked_posts, 'is_homepage': True})
